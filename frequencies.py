6e05fdabd8c877df192b435582ed8ebbdbd54f89
Mohammadgulzar Habibullah <mohammedgulzar79@gmail.com>
2023-09-24 15:04:13 +0100

    Completed Count frequencies Assignment

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
+
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if str(item) not in frequencies:
+            frequencies[str(item)] = 1
+        else:
+            frequencies[str(item)] = frequencies[str(item)] + 1
+
     return frequencies
